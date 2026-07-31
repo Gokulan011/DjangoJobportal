31b6daff1821a8bc8dd80d55aec60bf5655878d0
gokulan011 <gokulharish@034.com>
2026-07-31 14:42:38 +0530

    First commit

---
+++ backend/backend/views.py
@@ -0,0 +1,49 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User 
+from .models import Job , Application
+
+from .serializers import RegiterSerializer,JobSerializer,ApplicationSerializer
+
+@api_view(['GET'])
+def hello_api(request):
+    return Response({"message":"hello from django API"})
+
+@api_view(['POST'])
+def register_user(request):
+    serializer = RegiterSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()  #saves user
+        return Response({"message":"user Register Successfully"},status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def basic_login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    try:
+       user = User.objects.get(username=username,password=password)
+       return Response({"user_id" : user.id , "username": user.username , "message":"Login Successfull"},status=status.HTTP_200_OK)
+    except User.DoesNotExist:
+        return Response({"messsage":"invalid Credentials"},status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+def job_list(request):
+    jobs = Job.objects.all()
+    serializer = JobSerializer(jobs, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def apply_job(request):
+    serializer = ApplicationSerializer(data=request.data)
+    job_id = request.data.get("job")
+    applicant_id = request.data.get("applicant")
+    # check application exists
+    if Application.objects.filter(job_id=job_id,applicant_id=applicant_id).exists():
+        return Response({"message":"You Already have Applied!"},status=status.HTTP_400_BAD_REQUEST)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"message":"Application Submitted"},status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
